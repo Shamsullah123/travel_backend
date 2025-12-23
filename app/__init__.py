a16096dba4503b82999438ff917a31869839ff0c
Shamsullah123 <shamsullahkhan94@gmmail.com>
2025-12-23 03:23:06 -0800

    Add new Vercel deployment URL to CORS

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,8 @@ def create_app():
                  "http://localhost:5001", 
                  "http://127.0.0.1:5001", 
                  "https://travel-frontend-op8a82b9o-shamsullah123s-projects.vercel.app/",
-                 "https://travel-frontend-io2sdkmbp-shamsullah123s-projects.vercel.app"
+                 "https://travel-frontend-io2sdkmbp-shamsullah123s-projects.vercel.app",
+                 "https://travel-frontend-nc91xlo3p-shamsullah123s-projects.vercel.app"
              ],
              "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization", "X-Auth-Token"],
