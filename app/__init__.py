098d4ba0b7b49eeae3c40f25c4defe55877dbc43
Shamsullah123 <shamsullahkhan94@gmmail.com>
2025-12-23 03:33:03 -0800

    Final backend sync to GitHub

--- app/__init__.py
+++ app/__init__.py
@@ -25,9 +25,8 @@ def create_app():
                  "http://127.0.0.1:5000", 
                  "http://localhost:5001", 
                  "http://127.0.0.1:5001", 
-                 "https://travel-frontend-op8a82b9o-shamsullah123s-projects.vercel.app/",
-                 "https://travel-frontend-io2sdkmbp-shamsullah123s-projects.vercel.app",
-                 "https://travel-frontend-nc91xlo3p-shamsullah123s-projects.vercel.app"
+                 "https://travel-frontend-6qyzz76nh-shamsullah123s-projects.vercel.app",
+                 
              ],
              "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization", "X-Auth-Token"],
